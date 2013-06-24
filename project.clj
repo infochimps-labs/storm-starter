a7758f97d79a0383f1786b22b63df318e521f811
arrawatia <sumit.arrawatia@gmail.com>
2013-06-23 23:05:36 -0500

    Added the test rig with delay, fixed lein issues

--- project.clj
+++ project.clj
@@ -1,20 +1,29 @@
 (defproject storm-starter "0.0.1-SNAPSHOT"
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
+  :test-paths ["test/jvm"]
   :resource-paths ["multilang" "conf"]
   :aot :all
   :repositories {
 ;;                 "twitter4j" "http://twitter4j.org/maven2"
+                 "central-1" "http://repo1.maven.org/maven1"
+                 "central-2" "http://repo1.maven.org/maven2"
+                 "clojars" "http://clojars.org/repo/"
+                 "infochimps-releases" "https://s3.amazonaws.com/artifacts.chimpy.us/maven-s3p/releases"
+                 "infochimps-snapshots" "https://s3.amazonaws.com/artifacts.chimpy.us/maven-s3p/snapshots"
+                 "cloudera" "https://repository.cloudera.com/artifactory/cloudera-repos/"
                  }
 
   :dependencies [
 ;;                 [org.twitter4j/twitter4j-core "2.2.6-SNAPSHOT"]
 ;;                 [org.twitter4j/twitter4j-stream "2.2.6-SNAPSHOT"]
                    [commons-collections/commons-collections "3.2.1"]
+                   [storm/storm-kafka "0.9.0-wip16a-scala292"]
                  ]
 
   :profiles {:dev
               {:dependencies [[storm/storm-core "0.9.0-wip19-idea"]
+                              ;[org.easytesting/fest "1.0.16"]
                               [org.clojure/clojure "1.4.0"]]}}
   :min-lein-version "2.0.0"
   )
---
+++ src/jvm/com/infochimps/storm/testrig/util/KafkaLoader.java
@@ -0,0 +1,36 @@
+package com.infochimps.storm.testrig.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Properties;
+
+import kafka.javaapi.producer.Producer;
+import kafka.javaapi.producer.ProducerData;
+import kafka.producer.ProducerConfig;
+
+public class KafkaLoader {
+
+	public static void main(String[] args) throws Exception {
+
+		Properties props = new Properties();
+		props.put("zk.connect", "127.0.0.1:2181");
+		props.put("serializer.class", "kafka.serializer.StringEncoder");
+		ProducerConfig config = new ProducerConfig(props);
+		Producer<String, String> producer = new Producer<String, String>(config);
+
+		String filename = "/Users/sa/code/pagecounts-20080228-020001";
+		BufferedReader file = new BufferedReader(new FileReader(filename));
+		String line;
+		int length = 10;
+		for (int i = 0; i < length; i++) {
+
+			if ((line = file.readLine()) != null) {
+				System.out.println(line);
+				ProducerData<String, String> data = new ProducerData<String, String>(
+						"testrig3", line);
+				producer.send(data);
+			}
+		}
+		file.close();
+	}
+}
