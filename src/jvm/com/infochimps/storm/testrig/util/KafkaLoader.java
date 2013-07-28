c9e0865746d30f6eb4d4eb52eaaf9f6b93fab8a8
Philip (flip) Kromer <flip@infochimps.org>
2013-07-28 14:57:13 -0500

    reverting KafkaLoader

--- src/jvm/com/infochimps/storm/testrig/util/KafkaLoader.java
+++ src/jvm/com/infochimps/storm/testrig/util/KafkaLoader.java
@@ -2,7 +2,6 @@ package com.infochimps.storm.testrig.util;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.Properties;
 
 import kafka.javaapi.producer.Producer;
@@ -11,37 +10,27 @@ import kafka.producer.ProducerConfig;
 
 public class KafkaLoader {
 
-    public static final String START = "___START___";
-    public static final String STOP = "___STOP___";
-
     public static void main(String[] args) throws Exception {
 
         Properties props = new Properties();
         props.put("zk.connect", "127.0.0.1:2181");
         props.put("serializer.class", "kafka.serializer.StringEncoder");
-        props.put("partitioner.class", "com.infochimps.storm.testrig.util.StaticPartitioner");
-
         ProducerConfig config = new ProducerConfig(props);
-        Producer<Integer, String> producer = new Producer<Integer, String>(config);
+        Producer<String, String> producer = new Producer<String, String>(config);
 
-        String filename = "/Users/sa/code/data.txt";
+        String filename = "/Users/sa/code/pagecounts-20080228-020001";
         BufferedReader file = new BufferedReader(new FileReader(filename));
         String line;
-        int length = 3;
+        int length = 10;
         for (int i = 0; i < length; i++) {
-            ArrayList<String> a = new ArrayList<String>();
-            a.add(START);
-            while ((line = file.readLine()) != null) {
-                System.out.println(i + line);
-                a.add(i + line);
+
+            if ((line = file.readLine()) != null) {
+                System.out.println(line);
+                ProducerData<String, String> data = new ProducerData<String, String>(
+                        "testrig3", line);
+                producer.send(data);
             }
-            a.add(STOP);
-            ProducerData<Integer, String> data = new ProducerData<Integer, String>("testrig3", i, a);
-            producer.send(data);
-            file.close();
-            file = new BufferedReader(new FileReader(filename));
         }
         file.close();
     }
-
 }
