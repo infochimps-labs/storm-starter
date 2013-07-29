87c38c3b6af26da94666c4787567c67f037a84c6
Philip (flip) Kromer <flip@infochimps.org>
2013-07-28 23:28:40 -0500

    tying all versions to storm-0.9.0-ics -- storm, storm-kafka, etc.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storm-starter "0.0.1-SNAPSHOT"
+(defproject storm-starter "0.9.0-wip21-ics"
   :source-paths      ["src/clj"]
   :java-source-paths ["src/jvm"]
   :test-paths        ["test/jvm"]
@@ -12,17 +12,20 @@
                  "infochimps-snapshots" "https://s3.amazonaws.com/artifacts.chimpy.us/maven-s3p/snapshots"
                  "cloudera"             "https://repository.cloudera.com/artifactory/cloudera-repos/"
                  "github-releases"      "http://oss.sonatype.org/content/repositories/github-releases/"
-                 "twitter4j" 	        "http://twitter4j.org/maven2"
+                 "twitter4j"            "http://twitter4j.org/maven2"
                  }
 
   :dependencies [
                  [commons-collections/commons-collections "3.2.1"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.2.0"]
+                 [com.amazonaws/aws-java-sdk "1.3.27"]
                  ]
 
   :profiles {:dev
               { :resource-paths ["conf"]
-		:dependencies [[storm/storm-core                 "0.9.0-wip21"]
-                               [storm/storm-kafka                "0.9.0-wip16b-ics"]
+                :dependencies [[storm/storm-core                 "0.9.0-wip21-ics"]
+                               [storm/storm-kafka                "0.9.0-wip21-ics"]
+                               [storm/storm-util                 "1.7.0-SNAPSHOT"]
                                [junit/junit                      "3.8.1" :scope "test" ]
                                [org.testng/testng                "6.8" ]
                                [org.mockito/mockito-all          "1.9.0"]
