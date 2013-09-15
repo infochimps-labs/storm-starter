13f9dd0f2d887bc01f810f4bd7f79058b63d684f
arrawatia <sumit.arrawatia@gmail.com>
2013-09-15 09:56:43 -0700

    Add Kafka.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :source-paths      ["src/clj"]
   :java-source-paths ["src/jvm"]
   :test-paths        ["test/jvm"]
-  :resource-paths    ["multilang"]
+  :resource-paths    ["multilang", "src/jvm"]
   :aot :all
   :repositories {
                  "central-1"            "http://repo1.maven.org/maven1"
@@ -18,14 +18,15 @@
   :dependencies [
                  [commons-collections/commons-collections "3.2.1"]
                  [com.fasterxml.jackson.core/jackson-databind "2.2.0"]
+                               [org.apache.kafka/kafka-core                "0.7.1-incubating"]
                  [com.amazonaws/aws-java-sdk "1.3.27"]
                  ]
 
   :profiles {:dev
               { :resource-paths ["conf"]
                 :dependencies [[storm/storm-core                 "0.9.0-wip21-ics"]
-                               [storm/storm-kafka                "0.9.0-wip21-ics"]
-                               [com.infochimps/storm-util        "1.7.0-SNAPSHOT"]
+                               ;[storm/storm-kafka                "0.9.0-wip16a-scala292"]
+                              [com.infochimps/storm-util        "1.7.0-SNAPSHOT" :exclusions [[org.scala-lang/scala-library]]]
                                [junit/junit                      "3.8.1" :scope "test" ]
                                [org.testng/testng                "6.8" ]
                                [org.mockito/mockito-all          "1.9.0"]
